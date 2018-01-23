438d91dccb73a708bc250593091c55cafd350596
t3knoid <orion38>
2018-01-22 21:29:00 -0500

    Fix up format

--- m2tv/views.py
+++ m2tv/views.py
@@ -29,7 +29,7 @@ def index(request):
     dunawtvfeed = getm3u(dunawindex)
     citytvfeed = "https://citytv.hu/media/live/stream.m3u8"
 
-    message = """#EXTM3U\n#EXTINF: 1,Magyar M1\n%s\n#EXTINF: 2,Magyar M2\n%s\n#EXTINF: 3, Magyar M4\n%s\n#EXTINF: 4, Magyar M5\n%s\n#EXTINF: 5, Magyar Duna World\n%s\n#EXTINF: 6, Magyar Duna Live (Danube)\n%s\n#EXTINF: 7, CityTV\n%s\n"""
+    message = """#EXTM3U\n#EXTINF: 1,Magyar M1\n%s\n#EXTINF: 2,Magyar M2\n%s\n#EXTINF: 3, Magyar M4\n%s\n#EXTINF: 4, Magyar M5\n%s\n#EXTINF: 5, Magyar Duna World\n%s\n#EXTINF: 6, Magyar Duna Live (Danube)\n%s\n#EXTINF: 7, City TV\n%s\n"""
     return HttpResponse(message % (m1tvfeed, m2tvfeed, m4tvfeed, m5tvfeed, dunatvfeed, dunawtvfeed, citytvfeed))
 
 def getm3u(index):
